5eb581aa82aff5e3577849f76b81cca1444b62fd
Michael Mrazek <michael.mrazek@gmail.com>
2017-11-17 15:49:03 +0100

    Initial add

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -27,7 +27,7 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 echo "--- MGR_SSH: ${MGR_SSH} ---"
 
 # Create overlay network for default service discovery (if it doesn't already exist)
-ALL_NETWORKS=$(${MGR_SSH} docker network ls --quiet | wc -l)
+ALL_NETWORKS=$(${MGR_SSH} docker network ls)
 echo "All networks: ${ALL_NETWORKS}"
 
 NETWORKS=$(${MGR_SSH} docker network ls --filter name=service-discovery-network --quiet | wc -l)
