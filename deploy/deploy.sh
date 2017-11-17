0cf8a1258d799687e32f9b5787a18b3fc9f77fd1
Michael Mrazek <michael.mrazek@gmail.com>
2017-11-17 17:14:37 +0100

    Fixed clash in overlay network name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,28 +2,20 @@
 
 ##############################################################
 # Typical usage:
-# ./deploy.sh mgr-dns-name my-service 80:80 [aws-acct-number].dkr.ecr.[aws-region].amazonaws.com/[repo-name]/[image-name]:[image-tag] [my.pem]
-# Example:
-# ./deploy.sh ec2-52-59-209-188.eu-central-1.compute.amazonaws.com frontend-api 9000:9000 720340613074.dkr.ecr.eu-central-1.amazonaws.com/haaartland/frontend-api:1.0.6-f58d11b ~/.ssh/Docker_for_AWS_eu-central-1_KeyPair.pem
+# ./deploy.sh <mgr-dns-name> <docker-user> <docker-pass> <service-name> XXXX:YYYY [repo-name]/[image-name]:[image-tag]
 ##############################################################
 
 MGR_DNS_NAME=$1 # Public DNS name of one of the mgr nodes of the docker swarm cluster (in our case on AWS)
-SERVICE_NAME=$2 # The name to publish on the overlay network for the service (so that other services can call it internally within the swarm across EC2 nodes)
-PORTS=$3 # The port mapping for any external exposure for the service
-FULL_IMAGE_NAME=$4 # The full image name to deploy (in our case prefixed with the private ECR repo name)
-PEM=${5:-Empty} # PEM is typically: ~/.ssh/Docker_for_AWS_eu-central-1_KeyPair.pem (optional, if the script is run on CircleCI )
+DOCKER_USER=$2 # Docker Hub Username
+DOCKER_PASS=$3 # Docker Hub Password
+SERVICE_NAME=$4 # The name to use for the service on the overlay network
+PORTS=$5 # The port mapping for external exposure for the service
+FULL_IMAGE_NAME=$6 # The full image name to deploy (in our case prefixed with the private ECR repo name)
 
 echo ""
 echo "----- Deploying: $SERVICE_NAME to AWS docker swarm with manager node: $MGR_DNS_NAME -----"
 
-if [[ $PEM != Empty ]]  # Allowing optional parameter for .pem file
-then
-    MGR_SSH="ssh -o StrictHostKeyChecking=no -i ${PEM} docker@${MGR_DNS_NAME}"
-else
-    MGR_SSH="ssh -o StrictHostKeyChecking=no docker@${MGR_DNS_NAME}"
-fi
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
+MGR_SSH="ssh -o StrictHostKeyChecking=no docker@${MGR_DNS_NAME}"
 echo "--- MGR_SSH: ${MGR_SSH} ---"
 
 # Create overlay network for default service discovery (if it doesn't already exist)
@@ -35,6 +27,9 @@ if [[ "$NETWORKS" -eq 0 ]]; then
 	echo "Network api_service-discovery-network not found, creating..."
     ${MGR_SSH} docker network create -d overlay api_service-discovery-network
 fi
+
+# Login to DockerHub
+${MGR_SSH} docker login -u $DOCKER_USER -p $DOCKER_PASS
 
 SERVICE_ID=$(${MGR_SSH} docker service ls --filter name=${SERVICE_NAME} -q)
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
