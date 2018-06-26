809492bc633270bc2c69b9739185ef658c2a38df
Johannes Millegård <nour.johannes@gmail.com>
2018-06-26 15:57:16 +0200

    update deploy to ecs

--- deploy/ecs-deploy.sh
+++ deploy/ecs-deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#source .env.prod
-
 ECS_REGION=$1   # AWS region to deploy to
-ECR_NAME=$2     # Name of the service/application
-ENVIRONMENT=$3  # Name of the environment (e.g. canary, staging, production)
+ENVIRONMENT=$2  # Name of the environment (e.g. canary, staging, production)
+ECR_NAME=$3     # Name of the service/application
 HASH=$4         # Hash for the image version (e.g git commit hash)
 
+[[ -z "$ECS_REGION" ]] && { echo "must pass an region" ; exit 1; }
 [[ -z "$ECR_NAME" ]] && { echo "must pass a name" ; exit 1; }
 [[ -z "$ENVIRONMENT" ]] && { echo "must pass an environment" ; exit 1; }
+[[ -z "$HASH" ]] && { echo "must pass a hash" ; exit 1; }
 
 function getParameter() {
-
   local _val=$(aws ssm get-parameters --region $ECS_REGION --names "/$ENVIRONMENT/$1/$2" --with-decryption --query Parameters[0].Value)
   local _val=`echo $_val | sed -e 's/^"//' -e 's/"$//'`
   echo "$_val"
@@ -27,7 +26,6 @@ ECS_TASK_DEFINITION_NAME=$(getParameter "$ECR_NAME" "ECS_TASK_DEFINITION_NAME")
 ECS_SERVICE_NAME=$(getParameter "$ECR_NAME" "ECS_SERVICE_NAME")
 
 PORT=$(getParameter "$ECR_NAME" "PORT")
-
 COMMAND=$(getParameter "$ECR_NAME" "COMMAND")
 
 TASK_CPU=$(getParameter "$ECR_NAME" "TASK_CPU")
@@ -39,7 +37,6 @@ LAUNCH_TYPE=$(getParameter "$ECR_NAME" "LAUNCH_TYPE")
 
 EXECUTION_ROLE_ARN=$(getParameter "$ECR_NAME" "EXECUTION_ROLE_ARN")
 TASK_ROLE_ARN=$(getParameter "$ECR_NAME" "TASK_ROLE_ARN")
-
 
 VERSION=$ENVIRONMENT-$HASH
 
@@ -90,10 +87,9 @@ task_template='[
   }
 ]'
 
-echo "$task_template"
-
 task_def=$(printf "$task_template" $ECS_TASK_DEFINITION_NAME $AWS_ACCOUNT_ID $ECS_REGION $ECR_NAME $VERSION $TASK_CPU $TASK_MEMORY $TASK_MEMORY_RESERVATION $PORT $PORT $ENVIRONMENT $LOG_GROUP $ECS_REGION $LOG_PREFIX $ECR_NAME $COMMAND)
 
+echo "Register task definition:"
 echo "$task_def"
 
 # Register task definition
