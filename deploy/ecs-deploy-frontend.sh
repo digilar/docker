7d1e90e2704a5163998dd63cbd94a8d6e055a8fb
Eira Elwing <eira.elwing@digilar.se>
2018-09-05 13:51:14 +0200

    Add first attempt of aws parameters for frontend

---
+++ deploy/ecs-deploy-frontend.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ECS_REGION=$1   # AWS region to deploy to
+ENVIRONMENT=$2  # Name of the environment (e.g. canary, staging, production)
+ECR_NAME=$3     # Name of the service/application
+
+[[ -z "$ECS_REGION" ]] && { echo "must pass a region" ; exit 1; }
+[[ -z "$ECR_NAME" ]] && { echo "must pass a name" ; exit 1; }
+[[ -z "$ENVIRONMENT" ]] && { echo "must pass an environment" ; exit 1; }
+
+function getParameter() {
+  local _val=$(aws ssm get-parameters --region $ECS_REGION --names "/$ENVIRONMENT/$1/$2" --with-decryption --query Parameters[0].Value)
+  local _val=`echo $_val | sed -e 's/^"//' -e 's/"$//'`
+  echo "$_val"
+}
+
+# For both environments
+export API_ENDPOINT=$(getParameter "common" "API_ENDPOINT")
+
+# For both environments, but values can differ
+export AWS_DISTRIBUTION=$(getParameter "$ECR_NAME" "CLOUDFRONT_DISTRIBUTION")
+export AWS_BUCKET=$(getParameter "$ECR_NAME" "S3_BUCKET")
+export AWS_REGION=$(getParameter "$ECR_NAME" "S3_REGION")
+
+export AWS_ACCESS_KEY_ID=$(getParameter "$ECR_NAME" "S3_ACCESS_KEY_ID")
+export AWS_SECRET_ACCESS$(getParameter "$ECR_NAME" "S3_SECRET_ACCESS_KEY")
+
+export CLIENT_ID=$(getParameter "$ECR_NAME" "CLIENT_ID")
+export CLIENT_SECRET=$(getParameter "$ECR_NAME" "CLIENT_SECRET")
+
+# For single environment
+if [ "$ECR_NAME" = "arch-cms"]
+then 
+  export BABEL_ENDPOINT=$(getParameter "$ECR_NAME" "BABEL_ENDPOINT")
+fi 
+
+if [ "$ECR_NAME" = "babel-app"]
+then 
+  export ZENDESK_KEY=$(getParameter "$ECR_NAME" "ZENDESK_KEY")
+fi 
+
+echo "----- Deploying: $ECR_NAME to bucket: $AWS_BUCKET -----"
+echo "AWS_DISTRIBUTION: $AWS_DISTRIBUTION"
+echo "AWS_REGION: $AWS_REGION"
+
+ember deploy "$ENVIRONMENT" --activate
