697401c7399b0dbf485065e02db288cc3e4ac585
Eira Elwing <eira.elwing@digilar.se>
2019-06-17 14:56:48 +0200

    Return extra environment parameter

--- deploy/ecs-deploy-frontend.sh
+++ deploy/ecs-deploy-frontend.sh
@@ -5,7 +5,6 @@ ENVIRONMENT=$2  # Name of the environment (e.g. canary, staging, production)
 ECR_NAME=$3     # Name of the service/application
 ROOT_ENVIRONMENT=${ENVIRONMENT}
 
-
 [[ -z "$ECS_REGION" ]] && { echo "must pass a region" ; exit 1; }
 [[ -z "$ECR_NAME" ]] && { echo "must pass a name" ; exit 1; }
 [[ -z "$ENVIRONMENT" ]] && { echo "must pass an environment" ; exit 1; }
@@ -27,6 +26,8 @@ export AWS_REGION=$(getParameter "$ECR_NAME" "S3_REGION")
 export CLIENT_ID=$(getParameter "$ECR_NAME" "CLIENT_ID")
 export CLIENT_SECRET=$(getParameter "$ECR_NAME" "CLIENT_SECRET")
 
+export ENVIRONMENT_NAME=${ENVIRONMENT}
+
 # For single environment
 if [ "$ECR_NAME" = "arch-cms" ]
 then 
@@ -40,6 +41,7 @@ then
   export EMAIL_VALIDATOR_APIKEY=$(getParameter "$ECR_NAME" "EMAIL_VALIDATOR_APIKEY")
 fi 
 
+echo "Exported ENVIRONMENT_NAME: ${ENVIRONMENT_NAME}"
 echo "----- Deploying: $ECR_NAME to bucket: $AWS_BUCKET -----"
 echo "AWS_DISTRIBUTION: $AWS_DISTRIBUTION"
 echo "AWS_REGION: $AWS_REGION"
