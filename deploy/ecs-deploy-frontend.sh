5ac7936764eda9530a6f0006761a17f1a2d56610
Eira Elwing <eira.elwing@digilar.se>
2019-07-03 09:17:14 +0200

    Move env to function call

--- deploy/ecs-deploy-frontend.sh
+++ deploy/ecs-deploy-frontend.sh
@@ -9,32 +9,38 @@ ROOT_ENVIRONMENT=${ENVIRONMENT}
 [[ -z "$ECR_NAME" ]] && { echo "must pass a name" ; exit 1; }
 [[ -z "$ENVIRONMENT" ]] && { echo "must pass an environment" ; exit 1; }
 
+if [[ "${ENVIRONMENT}" == "production" ]]
+then
+  ENVIRONMENT=$(aws ssm get-parameter --region ${ECS_REGION} --name "/production/deploy/StackName" --query Parameter.Value)
+  ENVIRONMENT=`echo ${ENVIRONMENT} | sed -e 's/^"//' -e 's/"$//'`
+fi
+
 function getParameter {
-  local _val=$(aws ssm get-parameters --region ${ECS_REGION} --names "/$ENVIRONMENT/$1/$2" --with-decryption --query Parameters[0].Value)
+  local _val=$(aws ssm get-parameters --region ${ECS_REGION} --names "/$1/$2/$3" --with-decryption --query Parameters[0].Value)
   local _val=`echo ${_val} | sed -e 's/^"//' -e 's/"$//'`
   echo "$_val"
 }
 
 # For both environments
-export API_ENDPOINT=$(getParameter "common" "API_ENDPOINT")
+export API_ENDPOINT=$(getParameter "${ENVIRONMENT}" "common" "API_ENDPOINT")
 
 # For both environments, but values can differ
-export AWS_DISTRIBUTION=$(getParameter "$ECR_NAME" "CLOUDFRONT_DISTRIBUTION")
-export AWS_BUCKET=$(getParameter "$ECR_NAME" "S3_BUCKET")
-export AWS_REGION=$(getParameter "$ECR_NAME" "S3_REGION")
+export AWS_DISTRIBUTION=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "CLOUDFRONT_DISTRIBUTION")
+export AWS_BUCKET=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "S3_BUCKET")
+export AWS_REGION=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "S3_REGION")
 
-export CLIENT_ID=$(getParameter "$ECR_NAME" "CLIENT_ID")
-export CLIENT_SECRET=$(getParameter "$ECR_NAME" "CLIENT_SECRET")
+export CLIENT_ID=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "CLIENT_ID")
+export CLIENT_SECRET=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "CLIENT_SECRET")
 
 export ENVIRONMENT_NAME=${ROOT_ENVIRONMENT}
-export BABEL_ENDPOINT=$(getParameter "common" "BABEL_ENDPOINT")
+export BABEL_ENDPOINT=$(getParameter "${ENVIRONMENT}" "common" "BABEL_ENDPOINT")
 
 
 if [[ "$ECR_NAME" = "babel-app" ]]
 then 
-  export ZENDESK_KEY=$(getParameter "$ECR_NAME" "ZENDESK_KEY")
-  export EMAIL_VALIDATOR_LOCATION=$(getParameter "$ECR_NAME" "EMAIL_VALIDATOR_LOCATION")
-  export EMAIL_VALIDATOR_APIKEY=$(getParameter "$ECR_NAME" "EMAIL_VALIDATOR_APIKEY")
+  export ZENDESK_KEY=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "ZENDESK_KEY")
+  export EMAIL_VALIDATOR_LOCATION=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "EMAIL_VALIDATOR_LOCATION")
+  export EMAIL_VALIDATOR_APIKEY=$(getParameter "${ENVIRONMENT}" "$ECR_NAME" "EMAIL_VALIDATOR_APIKEY")
 fi 
 
 echo "Exported ENVIRONMENT_NAME: ${ENVIRONMENT_NAME}"
